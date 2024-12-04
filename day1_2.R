286a597628cf44d669255bab3c4938c3e0d0ea2c
gschone-data <gillesschone@gmail.com>
2024-12-04 11:52:55 +0100

    day1_2

---
+++ day1_2.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(dplyr)
+data <-read.delim("input",sep="",header=F)
+setDT(data)
+
+
+for (k in 1:2){ 
+temp<-data[,get(colnames(data)[k])]
+temp<-setDT(as.data.frame(temp))
+setorder(temp,temp)
+assign(paste0("temp",k),temp)
+}
+
+temp2 |> group_by(temp) |> summarise(n())->temp2_count
+head(temp2_count)
+
+temp1 |> inner_join(temp2_count,by="temp")->base_12
+base_12$similarity=base_12$temp*base_12$`n()`
+result_12=sum(base_12$similarity)
